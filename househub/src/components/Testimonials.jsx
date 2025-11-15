afddadb97d1f42cf73cfd62f146b28006f4c9b33
Joshua Iyaite <joshuaiyaite@gmail.com>
2025-11-15 11:37:51 +0300

    Refactor Testimonials component to use JSON data

--- househub/src/components/Testimonials.jsx
+++ househub/src/components/Testimonials.jsx
@@ -1,37 +1,22 @@
 import React from "react";
-import "./Testimonials.css";
+import "../styles/Testimonials.css";
+import data from "../data/testimonials.json";
 
-const testimonials = [
-  {
-    id: 1,
-    name: "Sarah Wanjiku",
-    text: "HouseHub helped me find my dream apartment very fast. Great service and great listings!",
-    image: require("../assets/pic1.jpg")
-  },
-  {
-    id: 2,
-    name: "Mark Otieno",
-    text: "I love how easy it was to browse homes. The customer support was also amazing!",
-    image: require("../assets/pic2.jpg")
-  }
-];
-
-const Testimonials = () => {
+function Testimonials() {
   return (
-    <section className="testimonials-section" id="testimonials">
-      <h2>What People Say</h2>
-
-      <div className="testimonials-grid">
-        {testimonials.map((item) => (
-          <div className="testimonial-card" key={item.id}>
-            <img src={item.image} alt={item.name} className="testimonial-image" />
-            <p className="testimonial-text">{item.text}</p>
-            <h4 className="testimonial-name">{item.name}</h4>
+    <section className="testimonials">
+      <h2>What Our Clients Say</h2>
+      <div className="testimonial-list">
+        {data.map((item) => (
+          <div key={item.id} className="testimonial-card">
+            <img src={process.env.PUBLIC_URL + item.image} alt={item.name} />
+            <p>"{item.comment}"</p>
+            <h4>- {item.name}</h4>
           </div>
         ))}
       </div>
     </section>
   );
-};
+}
 
 export default Testimonials;
